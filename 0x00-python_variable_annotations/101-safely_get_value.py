0ce6c06279446f7c3a0390a61b1b76af0325f66b
AlvyneZ <alvynew@gmail.com>
2023-12-27 19:50:31 +0300

    Correcting documentation

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -15,11 +15,13 @@ def safely_get_value(
         dct: Mapping, key: Any, default: DefType = None
         ) -> RetType:
     '''
-    Retrieves the first element of a sequence if it exists
+    Retrieves a value from a dictionary
 
-    lst(Sequence): the sequence
+    dct(Mapping): the dictionary to be searched through
+    key(Any): the key of the element to be retrieved
+    default(Any or None): return value if key is absent
 
-    Returns: first element or None
+    Returns: the element or default
     '''
     if key in dct:
         return dct[key]
