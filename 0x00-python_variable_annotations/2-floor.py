64f56107254b00c47ca45d4fab18e83a53f9d21e
AlvyneZ <alvynew@gmail.com>
2023-12-27 12:39:30 +0300

    converting data types

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+2-floor.py - Provides a method for getting the
+ floor of a float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Gets the floor of a float
+
+    n (float): float to be truncated
+
+    Returns: the floor
+    """
+    return int(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+3-to_str.py - Provides a method for getting the
+ string representation of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Gets the string representation of a float
+
+    n (float): float to be stringified
+
+    Returns: the string representation
+    """
+    return str(n)
